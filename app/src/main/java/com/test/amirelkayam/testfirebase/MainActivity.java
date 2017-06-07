b64791a5311bffd2fce2be3a554e0570031f5a28
Amir Elkayam <amir.elk116@gmail.com>
2017-06-07 18:24:10 +0300

    sbs project testing integrae with FireBase

---
+++ app/src/main/java/com/test/amirelkayam/testfirebase/MainActivity.java
@@ -0,0 +1,128 @@
+package com.test.amirelkayam.testfirebase;
+
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.ArrayMap;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatabaseReference mDatabase;
+    Button mButtonChange;
+    EditText mNameField;
+    EditText mEmailField;
+    EditText mPhoneField;
+    EditText mShowName;
+
+
+    Button mSuppliers;
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Customers");
+        mButtonChange = (Button) findViewById(R.id.change_btn);
+
+        mNameField = (EditText) findViewById(R.id.name_field);
+        mEmailField = (EditText) findViewById(R.id.email_field);
+        mPhoneField = (EditText)  findViewById(R.id.phone_field);
+        mShowName = (EditText) findViewById(R.id.show_name);
+
+
+        mSuppliers = (Button) findViewById(R.id.btn_suppliers);
+
+
+
+
+        mSuppliers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent_sup = new Intent(getApplicationContext(), Suppliers.class);
+                startActivity(intent_sup);
+            }
+        });
+
+
+/*
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                String showName = dataSnapshot.getValue().toString();
+                mShowName.setText("Name : " + showName);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+*/
+
+        mButtonChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String name = mNameField.getText().toString().trim();
+                String email = mEmailField.getText().toString().trim();
+                String phone = mPhoneField.getText().toString().trim();
+
+
+                HashMap<String, String> dataMap = new HashMap<String, String>();
+                dataMap.put("Name", name);
+                dataMap.put("Email", email);
+                dataMap.put("Phone", phone);
+
+
+                mDatabase.push().setValue(dataMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "Stored...", Toast.LENGTH_LONG).show();
+
+                        } else {
+                            Toast.makeText(MainActivity.this, "Error...", Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                });
+
+            }
+        });
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/test/amirelkayam/testfirebase/Suppliers.java
@@ -0,0 +1,111 @@
+package com.test.amirelkayam.testfirebase;
+
+import android.content.Context;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Suppliers extends AppCompatActivity {
+
+    DatabaseReference mDatabaseSuppliers;
+
+    Button mButtonFindSuppliers;
+    Button mButtonNewSuppliers;
+    ImageView mMainImage;
+    EditText mFindSuppliers;
+
+
+    Button mButtonSave;
+    EditText mNameField;
+    EditText mEmailField;
+    EditText mPhoneField;
+
+    final Context context = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suppliers);
+
+        mDatabaseSuppliers = FirebaseDatabase.getInstance().getReference().child("Suppliers");
+
+
+        mButtonFindSuppliers = (Button) findViewById(R.id.btn_findSuppliers);
+        mButtonNewSuppliers = (Button) findViewById(R.id.btn_newSuppliers);
+        mMainImage = (ImageView)findViewById(R.id.main_image);
+        mFindSuppliers = (EditText)findViewById(R.id.editText_findSuppliers);
+
+
+        mButtonSave = (Button) findViewById(R.id.save_btn);
+        mNameField = (EditText) findViewById(R.id.name_field);
+        mEmailField = (EditText) findViewById(R.id.email_field);
+        mPhoneField = (EditText)  findViewById(R.id.phone_field);
+
+/*
+        mButtonNewSuppliers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater li = LayoutInflater.from(context);
+                View customView = li.inflate(R.layout.inflater_new_supplier, null);
+            }
+        });
+*/
+        /// itsirat mazinim
+        View.OnClickListener ocBtn_findSuppliers =new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //TODO som Action
+                Toast.makeText(Suppliers.this, "ocBtn_findSuppliers", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+
+        View.OnClickListener ocBtn_newSuppliers =new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //TODO som Action
+                LayoutInflater li = LayoutInflater.from(context);
+                View customView = li.inflate(R.layout.inflater_new_supplier, null);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+                alertDialogBuilder.setView(customView);
+
+            }
+        };
+
+        View.OnClickListener oc_imageView =new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //TODO som Action
+                finish();
+             //   Toast.makeText(Suppliers.this, "oc_imageView", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        View.OnClickListener oc_editText_findSuppliers =new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //TODO som Action
+                Toast.makeText(Suppliers.this, "oc_editText_findSuppliers", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        /// afalat hakaftorim
+        mButtonFindSuppliers.setOnClickListener(ocBtn_findSuppliers);
+        mButtonNewSuppliers.setOnClickListener(ocBtn_newSuppliers);
+        mMainImage.setOnClickListener(oc_imageView);
+        mFindSuppliers.setOnClickListener(oc_editText_findSuppliers);
+
+
+    }
+}
